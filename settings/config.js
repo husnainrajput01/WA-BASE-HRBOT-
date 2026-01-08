adb801de74ad73f2932a46817f6d8083867bb66a
Husnain Rajput <141842546+husnainrajput01@users.noreply.github.com>
2026-01-08 23:11:57 +0500

    Add files via upload

---
+++ settings/config.js
@@ -0,0 +1,59 @@
+
+// 漏 2025 Debraj. All Rights Reserved.
+// respect the work, don鈥檛 just copy-paste.
+
+const fs = require('fs')
+
+const config = {
+    owner: "-",
+    botNumber: "-",
+    setPair: "K0MRAID1",
+    thumbUrl: "https://i.imgur.com/IkEv97P.jpeg",
+    session: "sessions",
+    status: {
+        public: true,
+        terminal: true,
+        reactsw: false
+    },
+    message: {
+        owner: "no, this is for owners only",
+        group: "this is for groups only",
+        admin: "this command is for admin only",
+        private: "this is specifically for private chat"
+    },
+    mess: {
+        owner: 'This command is only for the bot owner!',
+        done: 'Mode changed successfully!',
+        error: 'Something went wrong!',
+        wait: 'Please wait...'
+    },
+    settings: {
+        title: "Simple HR MD Bot",
+        packname: 'WA-BASE',
+        description: "this script was created by Husnain",
+        author: 'https://www.github.com/OfficialKango',
+        footer: "饾棈饾柧饾梾饾柧饾梹饾棆饾柡饾梿: @official_kango"
+    },
+    newsletter: {
+        name: "Simple WA Base Bot",
+        id: "0@newsletter"
+    },
+    api: {
+        baseurl: "https://hector-api.vercel.app/",
+        apikey: "hector"
+    },
+    sticker: {
+        packname: "Simple HR MD Bot",
+        author: "Husnain"
+    }
+}
+
+module.exports = config;
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+})
